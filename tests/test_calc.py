f50b9471e11f1602760960a37c0ff4bfc2f6f115
stellaglow1122 <wellmagg@gmail.com>
2025-03-28 22:23:01 +0800

    [Refactor] Changing file names

--- tests/test_calc.py
+++ tests/test_calc.py
@@ -1,11 +1,11 @@
 import unittest
-from calculator import Calculator  # The class we are going to implement
+from src.calculator import Calculator
 
 class TestCalculator(unittest.TestCase):
     def test_add(self):
         calc = Calculator()
         result = calc.add(2, 3)
-        self.assertEqual(result, 5)  # Expect 2 + 3 = 5
+        self.assertEqual(result, 5)
 
 if __name__ == "__main__":
     unittest.main()
