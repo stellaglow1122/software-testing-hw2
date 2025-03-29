dda052fb513bbf690c17d40593c4abb11e5f8b2f
stellaglow1122 <wellmagg@gmail.com>
2025-03-29 11:07:08 +0800

    [Fix] Adding type check in the implementation

--- src/calculator.py
+++ src/calculator.py
@@ -1,10 +1,16 @@
 class Calculator:
 
     def add(self, a, b):
+        if not (isinstance(a, (int, float)) and isinstance(b, (int, float))):
+            raise TypeError("Both arguments must be numbers")
         return a + b
 
     def minus(self, a, b):
+        if not (isinstance(a, (int, float)) and isinstance(b, (int, float))):
+            raise TypeError("Both arguments must be numbers")
         return a - b
 
     def multiply(self, a, b):
+        if not (isinstance(a, (int, float)) and isinstance(b, (int, float))):
+            raise TypeError("Both arguments must be numbers")
         return a * b
