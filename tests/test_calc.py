a1d636630f165f111b225087cc41ec968355e210
stellaglow1122 <wellmagg@gmail.com>
2025-03-29 10:36:25 +0800

    [Fix] Fixing the issues provided by flake8

--- tests/test_calc.py
+++ tests/test_calc.py
@@ -1,11 +1,13 @@
 import unittest
 from src.calculator import Calculator
 
+
 class TestCalculator(unittest.TestCase):
     def test_add(self):
         calc = Calculator()
         result = calc.add(2, 3)
         self.assertEqual(result, 5)
+
 
 if __name__ == "__main__":
     unittest.main()
