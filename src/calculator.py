77053381eeb812b5ce16bfb7d44ecc595bc8bc44
stellaglow1122 <wellmagg@gmail.com>
2025-03-29 11:37:02 +0800

    [Refactor] Creating validate_number function to reduce duplicate codes

--- src/calculator.py
+++ src/calculator.py
@@ -1,23 +1,24 @@
 class Calculator:
 
-    def add(self, a, b):
+    def validate_number(self, a, b):
+        """Utility method to validate if inputs are numbers."""
         if not (isinstance(a, (int, float)) and isinstance(b, (int, float))):
             raise TypeError("Both arguments must be numbers")
+
+    def add(self, a, b):
+        self.validate_number(a, b)
         return a + b
 
     def minus(self, a, b):
-        if not (isinstance(a, (int, float)) and isinstance(b, (int, float))):
-            raise TypeError("Both arguments must be numbers")
+        self.validate_number(a, b)
         return a - b
 
     def multiply(self, a, b):
-        if not (isinstance(a, (int, float)) and isinstance(b, (int, float))):
-            raise TypeError("Both arguments must be numbers")
+        self.validate_number(a, b)
         return a * b
 
     def divide(self, a, b):
-        if not (isinstance(a, (int, float)) and isinstance(b, (int, float))):
-            raise TypeError("Both arguments must be numbers")
+        self.validate_number(a, b)
         if b == 0:
             raise ZeroDivisionError("Division by zero is not allowed")
         return a / b
